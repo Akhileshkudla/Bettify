98fa65f5a3824d6553f6a2d60b2f2c0343fe6da0
mr.lazyProgrammer <akhilesh.k@siemens.com>
2023-10-10 22:41:48 +0530

    Formik implementation and DatePicker

--- client-app/src/features/activities/form/AcitivityForm.tsx
+++ client-app/src/features/activities/form/AcitivityForm.tsx
@@ -1,17 +1,24 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { Button, Form, Segment } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import { Button, Header, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
-import {v4 as uuid} from 'uuid';
+import { Formik, Form, } from "formik";
+import * as Yup from 'yup';
+import MyTextInput from "./MyTextInputs";
+import MyTextArea from "./MyTextArea";
+import MySelectInput from "./MySelectInput";
+import { categoryOptions } from "./options/categoryOptions";
+import MyDateInput from "./MyDateInput";
+import { v4 as uuid } from 'uuid';
 
 
-export default observer( function ActivityForm(){
-
-    const {activityStore } = useStore()
-    const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
+export default observer(function ActivityForm() {
+    
+    const { activityStore } = useStore()
+    const { updateActivity, createActivity, loading, loadActivity, loadingInitial } = activityStore;
 
     const {id} = useParams();
     const navigate = useNavigate()
@@ -21,46 +28,62 @@ export default observer( function ActivityForm(){
         title: '',
         category: '',
         description: '',
-        date: '',
-        city:'',
-        venue:''
+        date: null,
+        city: '',
+        venue: ''
     });
 
-    useEffect(()=> {
-        if(id) loadActivity(id).then(activity => setActivity(activity!));
+    const validationSchema = Yup.object({
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required('Date is required').nullable(),
+        city: Yup.string().required(),
+        venue: Yup.string().required(),
+    });
+
+    useEffect(() => {
+        if (id) loadActivity(id).then(activity => setActivity(activity!));
     }, [id, loadActivity])
 
-    function HandleSubmit(){
-        if(!activity.id)
-        {
+    function HandleFormSubmit(activity: Activity) {
+        if (!activity.id) {
             activity.id = uuid();
             createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
         }
-        else
-        {
+        else {
             updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
         }
     }
 
-    function HandleInputChange(event :ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        const {name, value} = event.target;
-        setActivity({...activity, [name]: value})
-    }
+    if (loadingInitial) return <LoadingComponent content="Loading..." />
 
-    if(loadingInitial) return <LoadingComponent content="Loading..." />
-
-    return(
+    return (
         <Segment clearing>
-            <Form onSubmit={HandleSubmit} autoComplete='off'>
-                <Form.Input placeholder='Title' value={activity.title} name='title' onChange={HandleInputChange}/>
-                <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={HandleInputChange}/>
-                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={HandleInputChange}/>
-                <Form.Input type="date" placeholder='Date' value={activity.date} name='date' onChange={HandleInputChange}/>
-                <Form.Input placeholder='City' value={activity.city} name='city' onChange={HandleInputChange}/>
-                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={HandleInputChange}/>
-                <Button loading={loading} floated="right" positive type="submit" content='Submit' />
-                <Button as={Link} to='/activities' floated="right" type="button" content='Cancel' />
-            </Form>
+            <Header content='Activity Details' sub color="teal" />
+            <Formik validationSchema={validationSchema} enableReinitialize initialValues={activity} onSubmit={values => HandleFormSubmit(values)}>
+                {({ handleSubmit, isValid, isSubmitting, dirty }) => (
+                    <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
+                        <MyTextInput name='title' placeholder="Title" />
+                        <MyTextArea rows={3} placeholder='Description' name='description' />
+                        <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
+                        <MyDateInput 
+                            placeholderText='Date' 
+                            name='date'
+                            showTimeSelect
+                            timeCaption="time"
+                            dateFormat='MMMM d, yyyy h:mm aa'
+                        />
+                        <Header content='Location Details' sub color="teal" />
+                        <MyTextInput placeholder='City' name='city' />
+                        <MyTextInput placeholder='Venue' name='venue' />
+                        <Button 
+                            disabled={isSubmitting || !dirty || !isValid }
+                            loading={loading} floated="right" positive type="submit" content='Submit' />
+                        <Button as={Link} to='/activities' floated="right" type="button" content='Cancel' />
+                    </Form>
+                )}
+            </Formik>
         </Segment>
     )
 })
---
+++ client-app/src/features/activities/form/options/categoryOptions.tsx
@@ -0,0 +1,6 @@
+export const categoryOptions= [
+    {text: 'Cricket', value:'cricket'},
+    {text: 'Office', value:'office'},
+    {text: 'Random', value:'Random'},
+    {text: 'Others', value:'others'},
+]
