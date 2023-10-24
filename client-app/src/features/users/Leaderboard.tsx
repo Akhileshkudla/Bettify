6c50e84c94d488ab16fc0482591409f26ef34df7
mr.lazyProgrammer <akhilesh.k@siemens.com>
2023-10-24 15:19:09 +0530

    Adding bar chart to Leaderboard

--- client-app/src/features/users/Leaderboard.tsx
+++ client-app/src/features/users/Leaderboard.tsx
@@ -2,6 +2,66 @@ import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
 import { Header, Table } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  indexAxis: 'y' as const,
+  elements: {
+    bar: {
+      borderWidth: 2,
+      radius: 5,
+      width: 2,
+      height: 2,
+      barPercentage: 0.8,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'bottom' as const,
+    },
+    title: {
+      display: true,      
+      text: 'Leaderboard',
+      font: {
+        size: 20,
+        family: 'Public Sans, sans-serif'
+      },
+      padding: {
+          top: 15,
+          bottom: 15
+      }
+    },
+  },
+  scales: {
+    y: {
+      ticks: {
+        font: {
+          size: 16, // Adjust the font size here
+          family: 'Public Sans, sans-serif',
+        },
+      },
+    },
+  },
+};
 
 export default observer (function UserForm() {
     const {userStore } = useStore();
@@ -14,7 +74,26 @@ export default observer (function UserForm() {
       // Sort the users array in decreasing order of 'amount'
   const sortedUsers = users.slice().sort((a, b) => b.amount - a.amount);
 
+  const filteredUsers = users.filter((user) => user.username !== 'admin')
+  const labels = filteredUsers.map((user) => user.displayName);
+  const data = filteredUsers.map((user) => user.amount);
+
+  const barData = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'User Amounts',
+        data: data,
+        borderColor: createGradient(),
+        backgroundColor: createGradient(), // Customize the border color
+        borderWidth: 1,        
+      },
+    ],
+  };
+
   return (
+    <>
+    <Bar color='teal' options={options} data={barData} />
     <Table celled>
       <Table.Header>
         <Table.Row>
@@ -38,5 +117,17 @@ export default observer (function UserForm() {
         ))}
       </Table.Body>
     </Table>
+    </>
   );
 })
+
+function createGradient(): CanvasGradient {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  const gradient = ctx!.createLinearGradient(0, 0, canvas.width *2, canvas.height);
+  gradient.addColorStop(0, 'red');
+  gradient.addColorStop(1, '#f5a640');
+
+  return gradient;
+}
