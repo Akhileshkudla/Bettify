6c599c48dfe1c3d478ca78d51686d72da098c069
mr.lazyProgrammer <akhilesh.k@siemens.com>
2023-11-06 12:06:54 +0530

    Fixed category issue

--- client-app/src/features/activities/form/options/categoryOptions.tsx
+++ client-app/src/features/activities/form/options/categoryOptions.tsx
@@ -1,6 +1,6 @@
 export const categoryOptions= [
     {text: 'Cricket', value:'cricket'},
     {text: 'Office', value:'office'},
-    {text: 'Random', value:'Random'},
+    {text: 'Random', value:'random'},
     {text: 'Others', value:'others'},
 ]
